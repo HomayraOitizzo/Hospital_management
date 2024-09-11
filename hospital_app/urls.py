ad6b249a610072491877efab56d6614ed45db480
HomayraOitizzo <hummuoitizzo@gmail.com>
2024-09-12 01:01:10 +0600

    login page part

--- hospital_app/urls.py
+++ hospital_app/urls.py
@@ -4,6 +4,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',home, name='home')
+    path('',home, name='home'),
+
+    path('login/', login_page, name="login"), 
+
+    path('register/', register_page, name='register')
 ]
 
--- hospital_app/views.py
+++ hospital_app/views.py
@@ -1,8 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user_objs = User.objects.filter(username = username)
+
+        if not user_objs.exists():
+            return render(request, 'login.html', {'error': "user not found"})
+        user_objs = authenticate(username = username, password = password)
+
+        if not user_objs:
+            context = {'error': 'Invalid password'}
+        return render(request, 'login.html', context)
     return render(request, 'login.html')
+
+
+def register_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user_objs = User.objects.filter(username = username)
+
+        if user_objs.exists():
+            return render(request, 'register.html', {'error1': 'user already exists'})
+        user = User.objects.create(username = username)
+
+        user.set_password(password)
+        user.save()
+        return redirect('/')        
+
+
+
+
+    return render(request, 'register.html')
+
+
